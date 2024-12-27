efb6ed9bdcdf81be0df87ec942eca2581eb534d5
sungwoongNoe <asdz453@gmail.com>
2024-12-27 17:50:22 +0900

    init

---
+++ src/main/java/com/multi/stockdiary/StockDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.multi.stockdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/stockdiary/StockDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.stockdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
